6ac14605db6cf23db2ffe7aa03d7d37551191f51
crccheck <c@crccheck.com>
2014-11-20 18:27:05 -0600

    ocd commit

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,34 +1,3 @@
-walk(document.body);
-
-function walk(node)
-{
-  // I stole this function from here:
-  // http://is.gd/mwZp7E
-
-  var child, next;
-
-  switch ( node.nodeType )
-  {
-    case 1:  // Element
-    case 9:  // Document
-    case 11: // Document fragment
-      child = node.firstChild;
-      while ( child )
-      {
-        next = child.nextSibling;
-        walk(child);
-        child = next;
-      }
-      break;
-
-    case 3: // Text node
-            if(node.parentElement.tagName.toLowerCase() != "script") {
-                handleText(node);
-            }
-      break;
-  }
-}
-
 function handleText(textNode) {
   // ['author', 'doctor', 'expert', 'farmer', 'lawyer', 'mayor', 'president', 'scientist', 'senator', 'veteran'].map(function (x) { return '[' + x[0].toUpperCase() + x[0].toLowerCase() + ']' + x.substr(1); }).join('|')
   // Case sensitive
@@ -37,3 +6,30 @@ function handleText(textNode) {
   v = v.replace(/\b([Aa]uthor|[Dd]octor|[Ee]xpert|[Ff]armer|[Ll]awyer|[Mm]ayor|[Pp]resident|[Ss]cientist|[Ss]enator|[Vv]eteran|Pope)(s)?\b/, "DJ$2");
   textNode.nodeValue = v;
 }
+
+function walk (node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+
+  var child, next;
+
+  switch (node.nodeType) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+    break;
+    case 3: // Text node
+      if (node.parentElement.tagName.toLowerCase() !== "script") {
+        handleText(node);
+      }
+    break;
+  }
+}
+
+walk(document.body);
